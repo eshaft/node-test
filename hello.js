293dd2e9fd11d529bc6126de0288b15e6194690b
Igor Kabozin <eshaft@gmail.com>
2018-06-07 21:37:46 +0300

    hello2

--- hello.js
+++ hello.js
@@ -4,7 +4,7 @@ http.createServer(function (req, res) {
     var name = require('url').parse(req.url, true).query.name;
     if (name === undefined) name = 'world';
     if (name == 'burningbird') {
-        var file = 'phoenix5a.png';
+        var file = 'node-test/phoenix5a.png';
         fs.stat(file, function (err, stat) {
             if (err) {
                 console.error(err);
