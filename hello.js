8cf50b3ee431f13b48f212120dfef2b36f2dbccf
Igor Kabozin <eshaft@gmail.com>
2018-06-08 18:22:05 +0300

    std

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+
 http.createServer(function (req, res) {
     var name = require('url').parse(req.url, true).query.name;
     if (name === undefined) name = 'world';
@@ -22,4 +23,17 @@ http.createServer(function (req, res) {
         res.end('Hello ' + name + '\n');
     }
 }).listen(8124);
+
 console.log('Server running at port 8124/');
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', function() {
+    var input = process.stdin.read();
+    if (input !== null) {
+        // Эхо-вывод текста
+        process.stdout.write(input);
+        var command = input.trim();
+        if (command == 'exit')
+            process.exit(0);
+    }
+});
